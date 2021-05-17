a74d8b74ef68ce17cdc09fc63f6437ba38223819
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-05-17 07:32:01 +0200

    add interface grading.ts = model data base and I have specified the data type

---
+++ src/app/models/grading.ts
@@ -0,0 +1,7 @@
+export interface Grading {
+    grade: string,
+    grade_description: string,
+    percent_from: string,
+    percent_to: string,
+    id: number
+}
--- src/app/service/grades.service.ts
+++ src/app/service/grades.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Grading } from '../models/grading';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GradesService {
 
+  private api_url = 'http://localhost:3000 + /grading';
   constructor(private http: HttpClient) { }
+
+  getGrades(): Observable<Grading[]> {
+    return this.http.get<Grading[]>(this.api_url).pipe(tap(console.log));
+  }
 }
